c1f3bb94ff49e247727e5cba653158c629d80593
Santosh Kumar <104520286+sanchanda@users.noreply.github.com>
2026-07-26 16:10:02 +0530

    Delete 10_chapter_Arrays/tempCodeRunnerFile.js

--- 10_chapter_Arrays/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-console.log(removed);
-
-// for (let i = 0; i < browser.length; i++) {
-//     console.log(browser[i]);
-//     if (browser[i] === "opera") {
-//         console.log("Opera doesn't support Automation Now!");
-//     }
-// }
